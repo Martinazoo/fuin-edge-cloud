6d7b468b5c127cbc23b6669357f4e049b7b87514
Martinazoo <paumartinnadal@gmail.com>
2025-04-04 09:31:38 +0200

    Hello World

---
+++ sensor.cpp
@@ -0,0 +1,26 @@
+#include "DHT.h" // DHT module library
+#define DHTPIN 3 // DHT11's data pin is connected to board's digital pin 3
+#define DHTTYPE DHT11 // DHT type
+DHT dht(DHTPIN, DHT11); // instantiate DHT object
+void setup(){
+  Serial.begin(9600);
+  dht.begin(); // initialize the sensor
+}
+void loop() {
+  float humidity = dht.readHumidity(); 
+  if (!isnan(humidity)) {
+    String humidity_str = String(humidity); //convert float to string
+    Serial.print("Humidity: " + humidity_str + ",");
+  } else {
+    Serial.println("Humidity reading from DHT11 sensor failed!");
+  }
+  float temperature = dht.readTemperature(); 
+  if (!isnan(temperature))
+  {
+    String temperature_str = String(temperature); //convert float to string
+    Serial.println("Temperature: " + temperature_str );
+  } else {
+    Serial.println("Temperature reading from DHT11 sensor failed!");
+  }
+  delay(1000);
+}
